5062fa1233157430108b499754a39d806dda4c8d
kavi2161 <144658492+kavi2161@users.noreply.github.com>
2023-09-11 12:32:23 +0530

    Add files via upload

---
+++ 11-09/mark.py
@@ -0,0 +1,13 @@
+name="kavi"
+address="Salem"
+doing="Msc"
+Tamil=80
+English=87
+Physics=78
+Maths=97
+Chemistry=90
+Computerscience=96
+total=Tamil+English+Physics+Maths+Chemistry+Computerscience
+avarage=total/600*100
+print("Name:", name, "Adress:" , address , "Doing:" , doing ,"Mark:" , Tamil,English,Physics,Maths,Chemistry,Computerscience,"Total:",total,"Avarage:",avarage)
+
